5e154774a06adea031408fde6f1e0a2f9a874f26
felipeInfante <afelipeinfante@gmail.com>
2025-05-18 13:17:37 -0300

    Screen Social Static Layout

--- src/screens/SocialScreen.js
+++ src/screens/SocialScreen.js
@@ -1,9 +1,56 @@
-import { View, Text } from 'react-native';
+import { View, Text, StyleSheet, ScrollView } from 'react-native';
+import Notification from '../components/Notification';
 
 export default function SocialScreen() {
   return (
-    <View style={{ flex: 1, justifyContent: 'center', alignItems: 'center' }}>
-      <Text>¡Social Screen XD!</Text>
-    </View>
+    <ScrollView style={styles.socialScreen}>
+      <View style={styles.socialHeader}>
+        <Text style={styles.headerTitle1}>Notifications</Text>
+        <Text style={styles.headerTitle2}>• Your Friends Activity</Text>
+      </View>
+      <View style={styles.notificationContainer}>
+        <Notification isNew={true}/>
+        <Notification isNew={true}/>
+        <Notification isNew={false}/>
+        <Notification isNew={false}/>
+        <Notification isNew={false}/>
+      </View>
+    </ScrollView>
   );
 }
+
+const styles = StyleSheet.create({
+
+  socialScreen: {
+    flexDirection: "column",
+    marginTop: 0,
+    backgroundColor: "#080D17"
+  },
+
+  socialHeader: {
+    flexDirection: "column",
+    alignItems: "flex-start",
+    marginLeft: 30,
+    marginTop: 30
+  },
+
+   headerTitle1: {
+    fontFamily: 'Roboto_900Black',
+    fontSize: 24,
+    color: '#FFFFFF',
+    paddingBottom: 5,
+  },
+
+  headerTitle2: {
+    fontFamily: 'Roboto_400Regular',
+    fontSize: 15,
+    color: '#FFFFFF',
+    paddingBottom: 5,
+  },
+
+  notificationContainer: {
+    flexDirection: "column",
+    alignItems: "center",
+    marginTop: 20,
+  }
+})
