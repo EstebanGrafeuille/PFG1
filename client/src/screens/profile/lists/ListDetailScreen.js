bac8c74bdfc1472248321d36772b84d2d6561e5a
felipeInfante <afelipeinfante@gmail.com>
2025-05-26 20:21:38 -0300

    list-detail-pre

--- client/src/screens/profile/lists/ListDetailScreen.js
+++ client/src/screens/profile/lists/ListDetailScreen.js
@@ -1,4 +1,4 @@
-import { View, Text, StyleSheet, Button, ScrollView, ActivityIndicator, SafeAreaView } from 'react-native';
+import { View, Text, StyleSheet, Button, Pressable, Image, ScrollView, ActivityIndicator, SafeAreaView } from 'react-native';
 import { useNavigation } from '@react-navigation/native';
 
 export default function ListDetailScreen(){
@@ -7,6 +7,21 @@ export default function ListDetailScreen(){
 
     return(
         <View style={styles.listDetailScreen}>
+            <View style={styles.listHeader}>
+                <Pressable onPress={() => navigation.navigate('ListDetailScreen')}>
+                    <View style={styles.buttonContainer}>
+                        <Image source={require("../../../../assets/img/back-icon-grey.png")} style={styles.backIcon}/>
+                    </View>
+                </Pressable>
+                <View style={styles.titleContainer}>
+                    <Text style={styles.title}>Literatura Rusa</Text>
+                </View>
+                <Pressable onPress={() => navigation.navigate('ListDetailScreen')}>
+                    <View style={styles.buttonContainer}>
+                        <Image source={require("../../../../assets/img/trash-icon-grey.png")} style={styles.backIcon}/>
+                    </View>
+                </Pressable>
+            </View>
             <Text>Detail List Screen</Text>
             <Button title="Go Back" onPress={() => navigation.navigate('ListsBooksScreen')}/>
         </View>
@@ -19,5 +34,9 @@ const styles = StyleSheet.create({
         flex: 1,
         justifyContent: "center",
         alignItems: "center"
-    }
+    },
+    listHeader: {
+        flexDirection: "row",
+        height: 80,
+    },
 })
