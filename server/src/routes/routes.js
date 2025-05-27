c13452bc49b47f43d2982ff7670f44536ab0dcca
Leanpe <leanperez2112@gmail.com>
2025-05-27 19:02:48 -0300

    UpdateImage

--- server/src/routes/routes.js
+++ server/src/routes/routes.js
@@ -2,14 +2,11 @@ const { Router } = require("express");
 const authRoutes = require("./auth");
 const bookRoutes = require("./books");
 const userRoutes = require("./users");
-const upload = require('../middleware/upload');
+
 const routes = Router();
 
 routes.use("/api/auth", authRoutes);
 routes.use("/api/books", bookRoutes);
 routes.use("/api/users", userRoutes);
-
-
-router.put('/:id/image', upload.single('image'), userController.updateImage);
 
 module.exports = routes;
--- server/src/routes/users.js
+++ server/src/routes/users.js
@@ -2,6 +2,7 @@ const express = require('express');
 const router = express.Router();
 const auth = require('../middleware/auth');
 const userController = require('../controllers/userController');
+const upload = require('../middleware/upload');
 
 // Todas las rutas requieren autenticación
 router.use(auth);
@@ -10,5 +11,6 @@ router.use(auth);
 router.get('/:id', userController.getById);
 router.get('/username/:username', userController.getByUsername);;
 router.put('/:id', userController.updateUser);
+router.put('/:id/image', upload.single('image'), userController.updateImage);
 
 module.exports = router;
