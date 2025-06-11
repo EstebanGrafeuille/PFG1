fb66844eda40a4dac1398917766941f5cb3d9cb2
felipeInfante <afelipeinfante@gmail.com>
2025-06-10 21:03:04 -0300

    Ajustar connection

--- client/src/services/connection.js
+++ client/src/services/connection.js
@@ -1,6 +1,4 @@
 // Usa la IP de tu máquina local en lugar de localhost para dispositivos físicos o emuladores
-//const BASE_URL = "https://d421-190-19-190-20.ngrok-free.app/api"; // IP que aparece en tus logs
-
-const BASE_URL = "http://192.168.0.122:5000/api"; // IP que aparece en tus logs
+const BASE_URL = "https://d421-190-19-190-20.ngrok-free.app/api"; // IP que aparece en tus logs
 
 export default BASE_URL;
