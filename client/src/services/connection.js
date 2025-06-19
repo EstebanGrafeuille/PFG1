58467721f5ff090de317faf75506e1586967f225
felipeInfante <afelipeinfante@gmail.com>
2025-06-18 21:12:26 -0300

    Localhost Felipe

--- client/src/services/connection.js
+++ client/src/services/connection.js
@@ -1,4 +1,5 @@
 // Usa la IP de tu máquina local en lugar de localhost para dispositivos físicos o emuladores
-const BASE_URL = process.env.BASE_URL || "http://192.168.100.6:5000/api"; // IP que aparece en tus logs
+// const BASE_URL = "http://192.168.100.6:5000/api"; // IP que aparece en tus logs
+const BASE_URL = "http://192.168.0.122:5000/api"; // Tu IP Personal
 
 export default BASE_URL;
